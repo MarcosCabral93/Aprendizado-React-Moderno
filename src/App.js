c3bf3fb145d0a73f05004c128a01e5712ae81bae
Marcos Paulo M Cabral <69735297+MarcosCabral93@users.noreply.github.com>
2021-08-29 08:45:41 -0300

    feat: este commit traz a implementação do UseEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, {useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+const[nome,usenome]=useState([]);
+const [input,useinput]=useState('');
+
+useEffect(()=>{
+  let local=localStorage.getItem('nome')
+  if(local){
+    usenome(JSON.parse(local))
+  }
+
+},[])
+
+useEffect(()=>{
+  //armazena no localstorage o array de nomes
+  localStorage.setItem('nome', JSON.stringify(nome));
+  //state que ele fica monitorando
+  },[nome])
+
+
+
+
+
+function handleAddNome(){
+  usenome([...nome,input])
+  useinput("")
+}
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text"  value={input} onChange={(e)=>useinput(e.target.value)}></input>
+      <button type="button" onClick={handleAddNome}> add nome</button>
+      <ul>
+        {nome.map((item)=>
+         
+            <li key={item}> {item}</li>
+        )}
+
+      </ul>
+      
     </div>
   );
 }
